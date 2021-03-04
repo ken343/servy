cea963dfc24d47396ec389ff7eb9eb3045573df3
ken343 <ken@dealwire.me>
2021-03-04 02:12:08 -0600

    Parsing Done

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -18,7 +18,7 @@ defmodule Servy.Handler do
 
   def route(conv) do
     # TODO: Create a new map that also has the response body:
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+    %{conv | resp_body: "Foxes"}
   end
 
   def format_response(conv) do
@@ -26,9 +26,9 @@ defmodule Servy.Handler do
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 
