5d77f30425eb6ce9f861910ab0378b7efccc7d75
ken343 <ken@dealwire.me>
2021-03-11 19:28:51 -0600

    Added Recursive Header Checking

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -9,15 +9,30 @@ defmodule Servy.Parser do
 
     [method, path, _] = String.split(request_line, " ")
 
+    headers = parse_headers(header_lines, %{})
+
     params = parse_params(params_string)
 
-    %Conv{ 
-       method: method, 
-       path: path,
-       params: params
-     }
+    IO.inspect header_lines
+
+    %Conv{
+      method: method,
+      path: path,
+      params: params,
+      headers: headers
+    }
+
   end
+
   def parse_params(params_string) do
     params_string |> String.trim |> URI.decode_query
   end
+
+  def parse_headers([head | tail], headers) do
+    [key, value] = String.split(head, ": ")
+    headers = Map.puts(headers, key, value)
+    parse_headers(tail, headers)
+  end
+
+  def parse_headers([], headers), do: headers
 end
