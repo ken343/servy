d6ce49eb1582ec560ed2ef5cd9595cd8a27647e7
ken343 <ken@dealwire.me>
2021-03-19 21:14:44 -0500

    Added Unit Testing and Fixed Issues

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -32,6 +32,6 @@ defmodule Servy.BearController do
     end
 
     def create(conv, %{"name" => name, "type" => type} = _params) do
-        %{ conv | status: 201, resp_body: "Create a #{name} bear named #{type}!"}
+        %{ conv | status: 201, resp_body: "Created a #{type} bear named #{name}!"}
     end
 end
